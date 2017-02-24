cfd75540026fb746ed3413c11a39e2e96b354b93
Lingzheng <lingzheng.yh@taobao.com>
2017-02-24 14:48:36 +0800

    Update: calculate bounding box of ellipses with transforms using calculus.

--- index.js
+++ index.js
@@ -128,15 +128,44 @@ function boundingRectOfEllipse(ellipse) {
   var matrix;
   if (transform) {
     matrix = _parseMatrixStr(transform);
-    newTrbl = _handleTransform(matrix, t, r, b, l);
+
+    // https://img.alicdn.com/tfscom/TB1iZqOPFXXXXceXpXXXXXXXXXX.jpg
+    var ma = matrix.a;
+    var mb = matrix.b;
+    var mc = matrix.c;
+    var md = matrix.d;
+    var me = matrix.e;
+    var mf = matrix.f;
+    var denominator = ma*md-mb*mc;
+    var A = ry*ry*md*md+rx*rx*mb*mb;
+    var B = -2*(mc*md*ry*ry+ma*mb*rx*rx);
+    var C = ry*ry*mc*mc+rx*rx*ma*ma;
+    var D = 2*ry*ry*(mc*md*mf-md*md*me)+2*rx*rx*(ma*mb*mf-mb*mb*me) - 2*(cx*ry*ry*md-cy*rx*rx*mb)*denominator;
+    var E = 2*ry*ry*(mc*md*me-mc*mc*mf)+2*rx*rx*(ma*mb*me-ma*ma*mf) + 2*(cx*ry*ry*mc-cy*rx*rx*ma)*denominator;
+    var F = ry*ry*(mc*mc*mf*mf-2*mc*md*me*mf+md*md*me*me)+rx*rx*(ma*ma*mf*mf-2*ma*mb*me*mf+mb*mb*me*me) + (2*cx*ry*ry*(md*me-mc*mf)+2*cy*rx*rx*(ma*mf-mb*me))*denominator + (ry*ry*cx*cx+rx*rx*cy*cy-rx*rx*ry*ry)*Math.pow(denominator, 2);
+    var a = 4*A*C-B*B;
+    var b1 = 4*A*E-2*B*D;
+    var c1 = 4*A*F-D*D;
+    var d1 = b1*b1-4*a*c1;
+    var b2 = 4*C*D-2*B*E;
+    var c2 = 4*C*F-E*E;
+    var d2 = b2*b2-4*a*c2;
+    var tb1 = (0-b1+Math.sqrt(d1))/(2*a);
+    var tb2 = (0-b1-Math.sqrt(d1))/(2*a);
+    var lr1 = (0-b2+Math.sqrt(d2))/(2*a);
+    var lr2 = (0-b2-Math.sqrt(d2))/(2*a);
     return {
-      left: newTrbl.l,
-      top: newTrbl.t,
-      right: newTrbl.r,
-      bottom: newTrbl.b,
-      width: newTrbl.r - newTrbl.l,
-      height: newTrbl.b - newTrbl.t
-    };
+      left: Math.min(lr1, lr2),
+      top: Math.min(tb1, tb2),
+      right: Math.max(lr1, lr2),
+      bottom: Math.max(tb1, tb2),
+      _wh: function() {
+        delete this._wh;
+        this.width = this.right - this.left;
+        this.height = this.bottom - this.top;
+        return this;
+      }
+    }._wh();
   }
 
   return {
